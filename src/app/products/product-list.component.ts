96ef7e1a8075dd199758ff8e81c58fdb8c4529ca
Fatos Idrizi <fatos.idrizi@gmail.com>
2020-04-08 18:12:00 +0200

    Create product-detail.component, router and routerlinks, navigation etc...

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {IProduct} from './product';
+import {ProductService} from './product.service';
 
 @Component({
   selector: 'pm-products',
@@ -7,42 +8,67 @@ import {IProduct} from './product';
   styleUrls: ['/product-list.component.css']
 })
 
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   imageWidth = 50;
   imageMargin = 2;
   showImage = false;
-  listFilter = 'cart';
-
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2019',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: 'assets/images/leaf_rake.png'
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2019',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: 'assets/images/garden_cart.png'
-    }
-  ];
+  products: IProduct[] = [];
+  errorMessage: string;
+
+  constructor(private productService: ProductService) {
+    // this.listFilter = 'cart';
+  }
+
+  private _listFilter;
+
+  get listFilter(): string {
+    return this._listFilter;
+  }
+
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performfilter(this.listFilter) : this.products;
+  }
 
   ngOnInit(): void {
     console.log('In OnInit');
+    this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
   }
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = 'Product List ' + message;
+  }
+
+  private performfilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    // const filteredArr = this.countProductsWithNameContains(this.products, filterBy);
+    // return filteredArr;
+    return this.products.filter(product =>
+      product.productName.toLowerCase().indexOf(filterBy) !== -1);
+  }
+
+  private countProductsWithNameContains(products: IProduct[], search: string): IProduct[] {
+    const filteredArr = [];
+    for (const p of products) {
+      if (p.productName.toLowerCase().indexOf(search) !== -1) {
+        filteredArr.push(p);
+      }
+    }
+    return filteredArr;
+  }
+
+
 }
