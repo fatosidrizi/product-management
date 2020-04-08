402eed08eb33acd589a68d2d47ce2728d6a237be
Fatos Idrizi <fatos.idrizi@gmail.com>
2020-04-08 18:44:08 +0200

    Created feature module product.module.ts, also created shared.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,38 +2,25 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
-import {ProductListComponent} from './products/product-list.component';
-import {FormsModule} from '@angular/forms';
 import {WelcomeComponent} from './home/welcome.component';
-import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
-import {StarComponent} from './shared/star.component';
 import {HttpClientModule} from '@angular/common/http';
-import {ProductDetailComponent} from './products/product-detail.component';
 import {RouterModule} from '@angular/router';
-import {ProductDetailGuard} from './products/product-detail.guard';
 import {ProductModule} from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent,
-    ProductListComponent,
-    StarComponent,
-    ConvertToSpacesPipe,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      {path: 'products', component: ProductListComponent},
-      {path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent},
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'},
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'},
     ]),
-    ProductModule
+    ProductModule,
   ],
   bootstrap: [AppComponent]
 })
